c16abe5305d5f8491f0b8c0bb048a81c1ab6c3e9
Steven Malikowski <cyclecoder@outlook.com>
2019-03-19 13:27:02 -0500

    Starting to add Entity

--- Week09 Assignment/SQL/DvdRepoEF01.sql
+++ Week09 Assignment/SQL/DvdRepoEF01.sql
@@ -37,7 +37,7 @@ values
 
 ('Jaws','1975','Speilberg','R','Shark has a toothy smile.'),
 ('Star Wars','1976','Lucas','PG','It is a space-age western.')
-
+('The Breakfast Club','1986','Hughs','PG','Detention humour.')
 
 select * from dvdMain
 GO
