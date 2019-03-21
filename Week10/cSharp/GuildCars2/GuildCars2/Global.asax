f46869e66ed4fa18b2d6d1f1476b95440bf6ca23
Steven Malikowski <cyclecoder@outlook.com>
2019-03-21 18:50:33 -0500

    Car db table script almost working, sans constraints

---
+++ Week10/cSharp/GuildCars2/GuildCars2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GuildCars2.MvcApplication" Language="C#" %>
---
+++ Week10/cSharp/GuildCars2/GuildCars2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GuildCars2.Startup))]
+namespace GuildCars2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
