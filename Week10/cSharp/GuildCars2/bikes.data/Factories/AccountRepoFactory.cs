f2c2508e21956252aaa0233afcc6deff0f9ed229
Steven Malikowski <cyclecoder@outlook.com>
2019-03-28 19:34:52 -0500

    Added users to db, but user repos are broke

---
+++ Week10/cSharp/GuildCars2/bikes.data/Factories/AccountRepoFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bikes.data.Factories
+{
+    public static class AccountRepoFactory
+    {
+        //public static IAccountRepository GetRepository()
+        //{
+        //    switch (Settings.GetRepositoryType())
+
+        //    {
+        //        case "ADO":
+        //            return new AccountRepoADO();
+        //        default:
+        //            throw new Exception("Could not find valid RepositoryType configuration value.");
+        //    }
+        //}
+    }
+}
